b0e136bda15ffcfcb2bb3146a9359b9183e7b235
ramble-hiker <10275485@uvu.edu>
2025-06-16 11:40:41 -0600

    test_all.py setup
    setup pytest test file.
    corrected import line from cli.py

---
+++ src/tests/test_all.py
@@ -0,0 +1,25 @@
+import pytest
+import sys
+import os
+
+# Get the parent directory
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+game_dir = parent_dir + "\\game"
+print("game_dir: ", game_dir)
+# Add parent directory to sys.path
+sys.path.append(parent_dir)
+# Add game directory to sys.path
+sys.path.append(game_dir)
+import main
+
+# Run tests:
+def test_main():
+    pass
+def test_cli():
+    pass
+def test_curses_cli():
+    pass
+def test_game_engine():
+    pass
+def test_Question():
+    pass
